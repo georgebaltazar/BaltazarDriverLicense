e20c3c205b9dfe25cd54a7240ad3859070880faa
georgebaltazar <georgebaltazar53@gmail.com>
2021-05-22 12:17:18 +0800

    uploaded

---
+++ GerogeDriverLicense/app/src/main/java/com/example/gerogedriverlicense/Nonedata.java
@@ -0,0 +1,20 @@
+package com.example.gerogedriverlicense;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Nonedata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nonedata);
+    }
+    public void back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
